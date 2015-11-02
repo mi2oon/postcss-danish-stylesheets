d9380b568c3b54e67d86c145d27e5a2197c18df7
Mithun Dhiman <mi2oon@gmail.com>
2015-11-02 18:56:06 +0100

    tree saving + linting

--- test/test.js
+++ test/test.js
@@ -4,33 +4,33 @@ import { expect } from 'chai';
 import plugin from '../';
 
 const test = (input, output, opts, done) => {
-	postcss([ plugin(opts) ])
-		.process(input)
-		.then(result => {
-			expect(result.css).to.eql(output);
-			expect(result.warnings()).to.be.empty;
-			done();
-		})
-		.catch(done);
+    postcss([ plugin(opts) ])
+        .process(input)
+        .then(result => {
+            expect(result.css).to.eql(output);
+            expect(result.warnings()).to.be.empty;
+            done();
+        })
+        .catch(done);
 };
 
 describe('postcss-danish-stylesheets', () => {
-	it('converted properties', done => {
-		test(
-			'a{ farve: xxx; }',
-			'a{ color: xxx; }',
-		{}, done);
-	});
-	it('converted values', done => {
-		test(
-			'a{ xxx: fed; }',
-			'a{ xxx: bold; }',
-		{}, done);
-	});
-	it('all', done => {
-		test(
-			'a{ xxx: fed!vigtig; farve: xxx; }',
-			'a{ xxx: bold !important; color: xxx; }',
-		{}, done);
-	});
+    it('converted properties', done => {
+        test(
+            'a{ farve: xxx; }',
+            'a{ color: xxx; }',
+        {}, done);
+    });
+    it('converted values', done => {
+        test(
+            'a{ xxx: fed; }',
+            'a{ xxx: bold; }',
+        {}, done);
+    });
+    it('all', done => {
+        test(
+            'a{ xxx: fed!vigtig; farve: xxx; }',
+            'a{ xxx: bold !important; color: xxx; }',
+        {}, done);
+    });
 });
